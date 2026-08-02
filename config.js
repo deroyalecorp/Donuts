5a77e0c37feebfdcef8be9a294e31aa87fb41c1f
deroyalecorp <deroyale.corp@gmail.com>
2026-08-02 16:16:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const defaultToppings = ["Keju", "Meses", "Kacang", "Crumble", "Marsh"];
 
 // Daftar 7 Varian Rasa Utama
 const baseFlavors = [
-    { name: "Coklat", img: "https://images.unsplash.com/photo-1551024601-bec78aea704b?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings },
-    { name: "Matcha", img: "https://images.unsplash.com/photo-1588643806968-3e4bce584749?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings },
-    { name: "Strawberry", img: "https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings },
-    { name: "Tiramisu", img: "https://images.unsplash.com/photo-1627308595229-7830f5c9c66e?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings },
-    { name: "Red Velvet", img: "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings },
-    { name: "Kacang", img: "https://images.unsplash.com/photo-1636248983944-f875b25f82ac?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings },
-    { name: "Taro", img: "https://images.unsplash.com/photo-1551024506-0bccd828d307?auto=format&fit=crop&w=400&q=80", toppings: defaultToppings }
+    { name: "Coklat", img: "images/coklat.jpeg", toppings: defaultToppings },
+    { name: "Matcha", img: "images/matcha.jpeg", toppings: defaultToppings },
+    { name: "Strawberry", img: "images/strawberry.jpeg", toppings: defaultToppings },
+    { name: "Tiramisu", img: "images/tiramisu.jpeg", toppings: defaultToppings },
+    { name: "Red Velvet", img: "images/red valvet.jpeg", toppings: defaultToppings },
+    { name: "Kacang", img: "images/kacang.jpeg", toppings: defaultToppings },
+    { name: "Taro", img: "images/taro.jpeg", toppings: defaultToppings }
 ];
 
 const generateItems = (prefix) => {
